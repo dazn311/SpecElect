0e6c41e16bf41b7d8cddf2a9aa3ff4546b6dfc57
dazn311 <57184332+dazn311@users.noreply.github.com>
2020-03-14 08:41:23 +0300

    first commit

--- config/web.php
+++ config/web.php
@@ -15,7 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '22hQIBJw1o6sQVYeYY59dZCnL76nTPaV',
-            'baseUrl' => '',
+            'baseUrl' => '', 
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
